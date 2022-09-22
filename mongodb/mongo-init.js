b039dd7cccffc3d3af90cef65846c6d6b85d9abc
CRodriguezUTEC <christopher.rodriguez@estudiantes.utec.edu.uy>
2022-09-22 15:37:48 -0300

    Commit incial en Dev

---
+++ mongodb/mongo-init.js
@@ -0,0 +1,9 @@
+db = db.getSiblingDB('sgr');
+db.createCollection('Personas');
+db.createCollection('PuntosDR');
+db.createCollection('Roles');
+db.createCollection('TipoDeResiduo');
+db.createCollection('Usuarios');
+db.createCollection('Vehiculos');
+db.createCollection('Zonas');
+
