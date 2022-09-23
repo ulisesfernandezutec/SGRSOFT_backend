04d9bc451c0f8d1b855b79d3d3c346573f70ed32
CRodriguezUTEC <christopher.rodriguez@estudiantes.utec.edu.uy>
2022-09-23 20:33:20 -0300

    Se agrega WebSecurityConfig para customizar la seguidad de las api

---
+++ com.sgr/src/main/java/com/sgr/bussines/WebSecurityConfig.java
@@ -0,0 +1,25 @@
+package com.sgr.bussines;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
+
+
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable()
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+}
