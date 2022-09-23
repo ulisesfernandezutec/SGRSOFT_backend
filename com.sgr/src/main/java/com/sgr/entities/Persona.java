89aedc6cfb3f9f7840e09019489c3b1922989170
CRodriguezUTEC <christopher.rodriguez@estudiantes.utec.edu.uy>
2022-09-23 20:18:33 -0300

    Commit de control

--- com.sgr/src/main/java/com/sgr/entities/Persona.java
+++ com.sgr/src/main/java/com/sgr/entities/Persona.java
@@ -2,7 +2,6 @@ package com.sgr.entities;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
