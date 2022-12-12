d149a8f5354c40dda6feb2df0998217ef7b6a4af
CRodriguezUTEC <christopher.rodriguez@estudiantes.utec.edu.uy>
2022-12-12 11:08:31 -0300

    Se cambia estado de Ruta y se elimina RutaPuntoEstado

--- com.sgr/src/main/java/com/sgr/api/interfaces/repository/RutaPuntoEstadoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sgr.api.interfaces.repository;
-
-import com.sgr.entities.RutaPuntoEstado;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface RutaPuntoEstadoRepository extends MongoRepository<RutaPuntoEstado,Long> {
-}
